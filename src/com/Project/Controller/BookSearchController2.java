403ec2a37cc6e5c57fe60575d947e667cdbc28d5
XuantongLu <luxuantong1994@gmail.com>
2017-05-23 00:34:47 +0200

    user interface

--- src/com/Project/Controller/BookSearchController2.java
+++ src/com/Project/Controller/BookSearchController2.java
@@ -95,7 +95,7 @@ public class BookSearchController2 implements Initializable {
                 System.out.println(rs.getString("Pub_name"));
                 System.out.println(rs.getString("author"));
                 System.out.println(rs.getDouble("price"));
-                System.out.println(rs.getString("type"));
+                System.out.println(rs.getString("type_name"));
                 System.out.println(rs.getInt("REPERTORY_SIZE"));
                 
                 bookData.add(new Book(
@@ -103,7 +103,7 @@ public class BookSearchController2 implements Initializable {
                         rs.getString("author"), 
                         rs.getDouble("price"), 
                         rs.getString("Pub_name"), 
-                        rs.getString("type"), 
+                        rs.getString("type_name"), 
                         rs.getInt("REPERTORY_SIZE"),
                         rs.getString("introduction")));
             }
@@ -133,14 +133,14 @@ public class BookSearchController2 implements Initializable {
                 System.out.println(rs.getString("Pub_name"));
                 System.out.println(rs.getString("author"));
                 System.out.println(rs.getDouble("price"));
-                System.out.println(rs.getString("type"));
+                System.out.println(rs.getString("type_name"));
                 
                 bookData.add(new Book(
                         rs.getString("book_name"), 
                         rs.getString("author"), 
                         rs.getDouble("price"), 
                         rs.getString("Pub_name"), 
-                        rs.getString("type"), 
+                        rs.getString("type_name"), 
                         rs.getInt("REPERTORY_SIZE"),
                         rs.getString("introduction")));
             }
@@ -203,7 +203,7 @@ public class BookSearchController2 implements Initializable {
                         rs.getString("author"), 
                         rs.getDouble("price"), 
                         rs.getString("Pub_name"), 
-                        rs.getString("type"), 
+                        rs.getString("type_name"), 
                         rs.getInt("REPERTORY_SIZE"),
                         rs.getString("introduction")));
             }
